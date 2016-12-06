32afda3c6f56a6a1d9c67bcff568f2e769360d77
chrispetsos <chrispetsos@gmail.com>
2016-12-06 14:41:19 +0200

    Created an initial testbed for TheoremProver.

---
+++ PaaSwordTheoremProver/src/org/seerc/paasword/theoremprover/TheoremProver.java
@@ -0,0 +1,5 @@
+package org.seerc.paasword.theoremprover;
+
+public class TheoremProver {
+
+}
---
+++ PaaSwordTheoremProver/test/org/seerc/paasword/theoremprover/test/TheoremProverTest.java
@@ -0,0 +1,35 @@
+package org.seerc.paasword.theoremprover.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.seerc.paasword.theoremprover.TheoremProver;
+
+public class TheoremProverTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCreate() {
+		TheoremProver tp = new TheoremProver();
+	}
+
+}
