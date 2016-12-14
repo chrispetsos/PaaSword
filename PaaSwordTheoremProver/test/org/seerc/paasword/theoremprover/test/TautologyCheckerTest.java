f11008dfd9415708c3eff0092960309ae004b86d
chrispetsos <chrispetsos@gmail.com>
2016-12-14 23:13:39 +0200

    Added a stress test for TautologyChecker.

--- PaaSwordTheoremProver/test/org/seerc/paasword/theoremprover/test/TautologyCheckerTest.java
+++ PaaSwordTheoremProver/test/org/seerc/paasword/theoremprover/test/TautologyCheckerTest.java
@@ -57,6 +57,143 @@ public class TautologyCheckerTest {
 		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr9"));
 		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr12"));
 		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr11"));
+		
+		// stress test
+		// ex1:expr
+		assertFalse(tc.isTautology("ex1:expr", "ex1:expr1"));
+		assertTrue(tc.isTautology("ex1:expr", "ex1:expr2"));
+		assertTrue(tc.isTautology("ex1:expr", "ex1:expr3"));
+		assertFalse(tc.isTautology("ex1:expr", "ex1:expr4"));
+		assertTrue(tc.isTautology("ex1:expr", "ex1:expr5"));
+		assertFalse(tc.isTautology("ex1:expr", "ex1:expr6"));
+		assertTrue(tc.isTautology("ex1:expr", "ex1:expr7"));
+		assertFalse(tc.isTautology("ex1:expr", "ex1:expr8"));
+		assertTrue(tc.isTautology("ex1:expr", "ex1:expr9"));
+		assertTrue(tc.isTautology("ex1:expr", "ex1:expr10"));
+		assertTrue(tc.isTautology("ex1:expr", "ex1:expr11"));
+		assertTrue(tc.isTautology("ex1:expr", "ex1:expr12"));
+		assertFalse(tc.isTautology("ex1:expr", "ex1:expr13"));
+		
+		// ex1:expr4
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr1"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr2"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr3"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr5"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr6"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr7"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr8"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr9"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr10"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr11"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr12"));
+		assertFalse(tc.isTautology("ex1:expr4", "ex1:expr13"));
+		
+		// ex1:expr6
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr"));
+		assertFalse(tc.isTautology("ex1:expr6", "ex1:expr1"));
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr2"));
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr3"));
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr4"));
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr5"));
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr7"));
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr8"));
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr9"));
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr10"));
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr11"));
+		assertTrue(tc.isTautology("ex1:expr6", "ex1:expr12"));
+		assertFalse(tc.isTautology("ex1:expr6", "ex1:expr13"));
+		
+		// ex1:expr8
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr"));
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr1"));
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr2"));
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr3"));
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr4"));
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr5"));
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr6"));
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr7"));
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr9"));
+		assertTrue(tc.isTautology("ex1:expr8", "ex1:expr10"));
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr11"));
+		assertTrue(tc.isTautology("ex1:expr8", "ex1:expr12"));
+		assertFalse(tc.isTautology("ex1:expr8", "ex1:expr13"));
+
+		// ex1:expr9
+		assertTrue(tc.isTautology("ex1:expr9", "ex1:expr"));
+		assertFalse(tc.isTautology("ex1:expr9", "ex1:expr1"));
+		assertTrue(tc.isTautology("ex1:expr9", "ex1:expr2"));
+		assertTrue(tc.isTautology("ex1:expr9", "ex1:expr3"));
+		assertFalse(tc.isTautology("ex1:expr9", "ex1:expr4"));
+		assertTrue(tc.isTautology("ex1:expr9", "ex1:expr5"));
+		assertFalse(tc.isTautology("ex1:expr9", "ex1:expr6"));
+		assertTrue(tc.isTautology("ex1:expr9", "ex1:expr7"));
+		assertFalse(tc.isTautology("ex1:expr9", "ex1:expr8"));
+		assertTrue(tc.isTautology("ex1:expr9", "ex1:expr10"));
+		assertTrue(tc.isTautology("ex1:expr9", "ex1:expr11"));
+		assertTrue(tc.isTautology("ex1:expr9", "ex1:expr12"));
+		assertFalse(tc.isTautology("ex1:expr9", "ex1:expr13"));
+
+		// ex1:expr10
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr1"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr2"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr3"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr4"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr5"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr6"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr7"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr8"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr9"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr11"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr12"));
+		assertFalse(tc.isTautology("ex1:expr10", "ex1:expr13"));
+
+		// ex1:expr11
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr1"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr2"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr3"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr4"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr5"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr6"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr7"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr8"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr9"));
+		assertTrue(tc.isTautology("ex1:expr11", "ex1:expr10"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr12"));
+		assertFalse(tc.isTautology("ex1:expr11", "ex1:expr13"));
+
+		// ex1:expr12
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr1"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr2"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr3"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr4"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr5"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr6"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr7"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr8"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr9"));
+		assertTrue(tc.isTautology("ex1:expr12", "ex1:expr10"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr11"));
+		assertFalse(tc.isTautology("ex1:expr12", "ex1:expr13"));
+
+		// ex1:expr1
+		assertTrue(tc.isTautology("ex1:expr1", "ex1:expr"));
+		assertTrue(tc.isTautology("ex1:expr1", "ex1:expr2"));
+		assertTrue(tc.isTautology("ex1:expr1", "ex1:expr3"));
+		assertFalse(tc.isTautology("ex1:expr1", "ex1:expr4"));
+		assertTrue(tc.isTautology("ex1:expr1", "ex1:expr5"));
+		assertFalse(tc.isTautology("ex1:expr1", "ex1:expr6"));
+		assertTrue(tc.isTautology("ex1:expr1", "ex1:expr7"));
+		assertFalse(tc.isTautology("ex1:expr1", "ex1:expr8"));
+		assertTrue(tc.isTautology("ex1:expr1", "ex1:expr9"));
+		assertTrue(tc.isTautology("ex1:expr1", "ex1:expr10"));
+		assertTrue(tc.isTautology("ex1:expr1", "ex1:expr11"));
+		assertTrue(tc.isTautology("ex1:expr1", "ex1:expr12"));
+		assertTrue(tc.isTautology("ex1:expr1", "ex1:expr13"));
+
 	}
 	
 	@Test
