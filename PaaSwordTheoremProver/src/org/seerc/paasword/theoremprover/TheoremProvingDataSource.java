1189d02868ad9aa3eafb9684074ea550a13653bf
chrispetsos <chrispetsos@gmail.com>
2016-12-21 15:15:47 +0200

    Added comments to TheoremProvingDataSource.

--- PaaSwordTheoremProver/src/org/seerc/paasword/theoremprover/TheoremProvingDataSource.java
+++ PaaSwordTheoremProver/src/org/seerc/paasword/theoremprover/TheoremProvingDataSource.java
@@ -6,12 +6,28 @@ import org.seerc.paasword.validator.engine.JenaDataSourceInferred;
 
 import com.hp.hpl.jena.ontology.OntModel;
 
+/**
+ * This class extends the JenaDataSourceInferred and after setting the model, it also
+ * adds the subsumption that a TautologyChecker has found. Note that this is assuming
+ * that the model behind this instance is immutable. This means that if the model
+ * changes after an object of a class has been created then no new potential
+ * subsumptions will be inferred. 
+ * @author Chris Petsos
+ *
+ */
 public class TheoremProvingDataSource extends JenaDataSourceInferred {
 	
+	/**
+	 * Create a TheoremProvingDataSource with an InputStream ontology.
+	 * @param is The ontology.
+	 */
 	public TheoremProvingDataSource(InputStream is) {
 		super(is);
 	}
 
+	/**
+	 * Upon setting the model, uses a TautologyChecker to add subsumptions.
+	 */
 	@Override
 	public void setModel(OntModel model) {
 		super.setModel(model);
