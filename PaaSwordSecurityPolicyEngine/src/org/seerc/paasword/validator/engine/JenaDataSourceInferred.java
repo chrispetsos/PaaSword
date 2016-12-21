bcbc4a2a5934b2a1b94afe2c9fd5774db7d4f4bc
chrispetsos <chrispetsos@gmail.com>
2016-12-21 12:19:01 +0200

    Added comments to JenaDataSourceInferred.

--- PaaSwordSecurityPolicyEngine/src/org/seerc/paasword/validator/engine/JenaDataSourceInferred.java
+++ PaaSwordSecurityPolicyEngine/src/org/seerc/paasword/validator/engine/JenaDataSourceInferred.java
@@ -8,6 +8,12 @@ import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 
+/**
+ * Class that extends JenaDataSource and uses the Pellet reasoner for inferences.
+ * 
+ * @author Chris Petsos
+ *
+ */
 public class JenaDataSourceInferred extends JenaDataSource {
 
 	public JenaDataSourceInferred(OntModel model) {
@@ -19,6 +25,9 @@ public class JenaDataSourceInferred extends JenaDataSource {
 		super(is);
 	}
 
+	/**
+	 * When setting the model wrap it with one that uses the Pellet reasoner.
+	 */
 	@Override
 	public void setModel(OntModel model) {
 		super.setModel(ModelFactory.createOntologyModel(PelletReasonerFactory.THE_SPEC, model));
