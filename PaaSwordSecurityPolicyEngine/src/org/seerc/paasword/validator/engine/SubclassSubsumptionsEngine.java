534e536737e3159a5a7ddd682f60146127844356
chrispetsos <chrispetsos@gmail.com>
2017-02-02 13:31:09 +0200

    Added support for pac:ORContextExpression and pac:refersTo in SubclassSubsumptionsEngine.

--- PaaSwordSecurityPolicyEngine/src/org/seerc/paasword/validator/engine/SubclassSubsumptionsEngine.java
+++ PaaSwordSecurityPolicyEngine/src/org/seerc/paasword/validator/engine/SubclassSubsumptionsEngine.java
@@ -6,7 +6,10 @@ import java.util.List;
 import com.hp.hpl.jena.ontology.HasValueRestriction;
 import com.hp.hpl.jena.ontology.Individual;
 import com.hp.hpl.jena.ontology.IntersectionClass;
+import com.hp.hpl.jena.ontology.OntClass;
 import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.UnionClass;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFList;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
@@ -32,65 +35,101 @@ public class SubclassSubsumptionsEngine extends EntitySubsumptionBaseEngine {
 		// Iterate over them. 
 		for(Individual individual:individualsIterator)
 		{
-			// Get the reference statement. Should be at most one... 
-			Resource individualReference = null;
+			// Get the reference statement iterator. Should have at most one element... 
 			StmtIterator referenceStatementIterator = individual.listProperties(ResourceFactory.createProperty(jdsi.createResourceFromUri("otp:TheoremProvingReferenceProperty").getURI()));
-			if(referenceStatementIterator.hasNext())
-			{
-				Statement referenceStatement = referenceStatementIterator.next();
-				individualReference = referenceStatement.getObject().asResource();
-			}
 			
 			// Get the statements where the individual is subject of a "otp:TheoremProvingParameterProperty" parameter.
 			StmtIterator resourceParams = individual.listProperties(ResourceFactory.createProperty(jdsi.createResourceFromUri("otp:TheoremProvingParameterProperty").getURI()));
 
+			// the class, either intersection or union etc., with the parameter restrictions.
+			OntClass parameterRestrictionClass = null;
+			
+			// the class to which this individual will become equivalent
+			OntClass equivalentClass = null;
+			
 			// Switch cases of individual
 			if(individual.hasOntClass(jdsi.createResourceFromUri("otp:ANDTheoremProvingClass").getURI()))
 			{	// otp:ANDTheoremProvingClass
-				this.createIntersectionRestriction(individual, resourceParams);
+				parameterRestrictionClass = this.createIntersectionRestriction(resourceParams);
 			}
 			else if(individual.hasOntClass(jdsi.createResourceFromUri("otp:ORTheoremProvingClass").getURI()))
 			{	// otp:ORTheoremProvingClass
-
+				parameterRestrictionClass = this.createUnionRestriction(resourceParams);
 			}
 			else if(individual.hasOntClass(jdsi.createResourceFromUri("otp:XORTheoremProvingClass").getURI()))
 			{	// otp:XORTheoremProvingClass
-
+				// TODO
 			}
 			else if(individual.hasOntClass(jdsi.createResourceFromUri("otp:NOTheoremProvingClass").getURI()))
 			{	// otp:NOTheoremProvingClass
-
+				// TODO
 			}
-			else
-			{	// "terminating" param
-
+			
+			equivalentClass = parameterRestrictionClass;
+			
+			if(referenceStatementIterator.hasNext())
+			{	// this expression has reference
+				// create intersection of parameterRestrictionClass and referenceStatement
+				Statement referenceStatement = referenceStatementIterator.next();
+				equivalentClass = this.createReferenceIntersection(equivalentClass, referenceStatement);
 			}
+			
+			// make the individual also a class and equivalent to the desired class
+			((OntModel)this.jdsi.getModel()).createClass(individual.getURI()).addEquivalentClass(equivalentClass);
+
 		}
 	}
 
-	private void createIntersectionRestriction(Individual individual, StmtIterator resourceParams)
+	private OntClass createIntersectionRestriction(StmtIterator resourceParams)
 	{
 		// the hasValue Restrictions RDFList
+		RDFList hasValueRestrictionsRDFList = createHasValueRestrictionsRDFList(resourceParams);
+		
+		// create the intersection class and return it
+		return ((OntModel)this.jdsi.getModel()).createIntersectionClass(null, hasValueRestrictionsRDFList);
+	}
+
+	private OntClass createUnionRestriction(StmtIterator resourceParams)
+	{
+		// the hasValue Restrictions RDFList
+		RDFList hasValueRestrictionsRDFList = createHasValueRestrictionsRDFList(resourceParams);
+		
+		// create the union class and return it
+		return ((OntModel)this.jdsi.getModel()).createUnionClass(null, hasValueRestrictionsRDFList);
+		
+	}
+
+	private RDFList createHasValueRestrictionsRDFList(StmtIterator valueStatements) {
 		RDFList hasValueRestrictionsRDFList;
 		
 		// first put them in a List
 		List<RDFNode> hasValueRestrictionsList = new ArrayList<RDFNode>();
-		while(resourceParams.hasNext())
+		while(valueStatements.hasNext())
 		{
-			RDFNode param = resourceParams.next().getObject();
-			HasValueRestriction hvr = ((OntModel)this.jdsi.getModel()).createHasValueRestriction(null, ResourceFactory.createProperty(jdsi.createResourceFromUri("otp:TheoremProvingParameterProperty").getURI()), param);
+			Statement statement = valueStatements.next();
+			Property valueProperty = statement.getPredicate();
+			RDFNode value = statement.getObject();
+			HasValueRestriction hvr = ((OntModel)this.jdsi.getModel()).createHasValueRestriction(null, valueProperty, value);
 			hasValueRestrictionsList.add(hvr);
 		}
 
 		// then create the RDFList
 		hasValueRestrictionsRDFList = ((OntModel)this.jdsi.getModel()).createList(hasValueRestrictionsList.iterator());
-		
-		// create the intersection class
-		IntersectionClass intersection = ((OntModel)this.jdsi.getModel()).createIntersectionClass(null, hasValueRestrictionsRDFList);
-		
-		// make the individual also a class and equivalent to the intersection
-		((OntModel)this.jdsi.getModel()).createClass(individual.getURI()).addEquivalentClass(intersection);
+		return hasValueRestrictionsRDFList;
 	}
+
+	private OntClass createReferenceIntersection(OntClass parameterRestrictionClass, Statement referenceStatement)
+	{
+		List<RDFNode> referenceIntersectionList = new ArrayList<RDFNode>();
+		referenceIntersectionList.add(parameterRestrictionClass);
+		HasValueRestriction referenceHasValueRestriction = ((OntModel)this.jdsi.getModel()).createHasValueRestriction(null, referenceStatement.getPredicate(), referenceStatement.getObject());
+		referenceIntersectionList.add(referenceHasValueRestriction);
+		
+		RDFList referenceIntersectionRDFList = ((OntModel)this.jdsi.getModel()).createList(referenceIntersectionList.iterator());
+		
+		return ((OntModel)this.jdsi.getModel()).createIntersectionClass(null, referenceIntersectionRDFList);
+	}
+
 
 	@Override
 	protected boolean entitySubsumes(String entity1Uri, String entity2Uri) {
