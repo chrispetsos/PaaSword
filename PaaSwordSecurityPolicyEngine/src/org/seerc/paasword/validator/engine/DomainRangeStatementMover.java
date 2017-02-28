197bf81506139abfc7a11fa675ac50df86239999
chrispetsos <chrispetsos@gmail.com>
2017-02-28 15:07:24 +0200

    Created DomainRangeStatementMover which takes the domain/range statements of a data source and moves them to a target Jena model.

---
+++ PaaSwordSecurityPolicyEngine/src/org/seerc/paasword/validator/engine/DomainRangeStatementMover.java
@@ -0,0 +1,52 @@
+package org.seerc.paasword.validator.engine;
+
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFList;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+
+public class DomainRangeStatementMover {
+
+	public void moveDomainRangeStatements(JenaDataSource dataSource, OntModel targetModel)
+	{
+		this.movePropertyStatements(dataSource, targetModel, "http://www.w3.org/2000/01/rdf-schema#domain");
+		this.movePropertyStatements(dataSource, targetModel, "http://www.w3.org/2000/01/rdf-schema#range");
+	}
+
+	private void movePropertyStatements(JenaDataSource dataSource, OntModel targetModel, String propertyUri) {
+		// create an OWL DL source model to work with
+		OntModel sourceModel =  ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
+		sourceModel.add(dataSource.getModel().listStatements());
+		
+		// find all subjects of property
+		Property property = sourceModel.createProperty(propertyUri);
+		ResIterator subjectsOfPropertyStatements = sourceModel.listSubjectsWithProperty(property);
+		while(subjectsOfPropertyStatements.hasNext())
+		{
+			Resource subjectOfProperty = subjectsOfPropertyStatements.next();
+			// find all property statements where subjectOfProperty is subject
+			StmtIterator propertyStatements = sourceModel.listStatements(subjectOfProperty, property, (RDFNode)null);
+			// The RDFList that will hold all objects of the statements
+			RDFList propertyList = targetModel.createList();
+			while(propertyStatements.hasNext())
+			{
+				Statement propertyStatement = propertyStatements.next();
+				propertyList = propertyList.with(propertyStatement.getObject());
+			}
+			// create the union class that will hold all statements
+			OntClass unionClass = targetModel.createUnionClass(null, propertyList);
+			// add the property statement with the union class in targetModel
+			targetModel.add(subjectOfProperty, property, unionClass);
+			// remove propertyStatements from dataSource
+			dataSource.getModel().remove(dataSource.getModel().listStatements(subjectOfProperty, property, (RDFNode)null));
+		}
+	}
+
+}
---
+++ PaaSwordSecurityPolicyEngine/test/org/seerc/paasword/validator/engine/test/DomainRangeStatementMoverTest.java
@@ -0,0 +1,94 @@
+package org.seerc.paasword.validator.engine.test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mindswap.pellet.jena.PelletReasonerFactory;
+import org.seerc.paasword.validator.engine.DomainRangeStatementMover;
+import org.seerc.paasword.validator.engine.JenaDataSource;
+import org.seerc.paasword.validator.engine.JenaDataSourceInferred;
+
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.ontology.UnionClass;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFList;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+
+public class DomainRangeStatementMoverTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testMoveDomainRangeStatements() throws Exception {
+		JenaDataSourceInferred jdsi = new JenaDataSourceInferred(new FileInputStream(new File("Ontologies/test/DomainRangeMoveTest.ttl")));
+		
+		// we have 4 domain and 4 range statements added by the reasoner for top and bottom object properties
+		
+		// jds should have 4+3 domain statements
+		assertEquals(7, this.getStatementsWithProperty((OntModel) jdsi.getModel(), "http://www.w3.org/2000/01/rdf-schema#domain").size());
+		
+		// jds should have 4+2 range statements
+		assertEquals(6, this.getStatementsWithProperty((OntModel) jdsi.getModel(), "http://www.w3.org/2000/01/rdf-schema#range").size());
+
+		DomainRangeStatementMover drsm = new DomainRangeStatementMover();
+		
+		OntModel targetModel = ModelFactory.createOntologyModel(PelletReasonerFactory.THE_SPEC);
+
+		drsm.moveDomainRangeStatements(jdsi, targetModel);
+		
+		// jds should have no domain/range statements
+		assertEquals(0, this.getStatementsWithProperty((OntModel) jdsi.getModel(), "http://www.w3.org/2000/01/rdf-schema#domain").size());
+		assertEquals(0, this.getStatementsWithProperty((OntModel) jdsi.getModel(), "http://www.w3.org/2000/01/rdf-schema#range").size());
+		
+		// Find the domain of ex1:aProperty
+		RDFNode domainA = targetModel.listStatements(targetModel.createResource("http://www.paasword.eu/security-policy/use-cases/car-park#aProperty"), targetModel.createProperty("http://www.w3.org/2000/01/rdf-schema#domain"), (RDFNode)null).next().getObject();
+		// Create the Union of ex1:ClassA and ex1:ClassB
+		RDFList classList = targetModel.createList(new RDFNode[] {targetModel.createClass("http://www.paasword.eu/security-policy/use-cases/car-park#ClassA"), targetModel.createClass("http://www.paasword.eu/security-policy/use-cases/car-park#ClassB")});
+		// Domain of ex1:aProperty should be equivalent to the Union
+		assertTrue(domainA.as(OntClass.class).hasEquivalentClass(targetModel.createUnionClass("", classList)));
+
+		// Same for domain of pcm:isLocatedIn
+		RDFNode domainB = targetModel.listStatements(targetModel.createResource("http://www.paasword-project.eu/ontologies/casm/2016/05/20#isLocatedIn"), targetModel.createProperty("http://www.w3.org/2000/01/rdf-schema#domain"), (RDFNode)null).next().getObject();
+		assertTrue(domainB.as(OntClass.class).hasEquivalentClass(targetModel.createClass("http://www.paasword-project.eu/ontologies/casm/2016/05/20#Area")));
+
+		// Same for range of ex1:aProperty
+		RDFNode rangeA = targetModel.listStatements(targetModel.createResource("http://www.paasword.eu/security-policy/use-cases/car-park#aProperty"), targetModel.createProperty("http://www.w3.org/2000/01/rdf-schema#range"), (RDFNode)null).next().getObject();
+		assertTrue(rangeA.as(OntClass.class).hasEquivalentClass(targetModel.createClass("http://www.paasword.eu/security-policy/use-cases/car-park#ClassC")));
+
+		// And range of pcm:isLocatedIn
+		RDFNode rangeB = targetModel.listStatements(targetModel.createResource("http://www.paasword-project.eu/ontologies/casm/2016/05/20#isLocatedIn"), targetModel.createProperty("http://www.w3.org/2000/01/rdf-schema#range"), (RDFNode)null).next().getObject();
+		assertTrue(rangeB.as(OntClass.class).hasEquivalentClass(targetModel.createClass("http://www.paasword-project.eu/ontologies/casm/2016/05/20#Area")));
+	}
+
+	private List<Statement> getStatementsWithProperty(OntModel model, String property)
+	{
+		return model.listStatements((Resource)null, model.createProperty(property), (RDFNode)null).toList();
+	}
+}
