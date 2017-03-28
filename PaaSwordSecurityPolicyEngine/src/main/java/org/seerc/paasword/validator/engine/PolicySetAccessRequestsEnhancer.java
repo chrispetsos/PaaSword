29d7bcae67845fc6db8174cc9245b2953907f878
chrispetsos <chrispetsos@gmail.com>
2017-03-28 17:28:11 +0300

    Differentiated the created class instances for policy sets' access requests from the ones of policies' access requests. In PolicySetAccessRequestsEnhancer, instead of throwing runtime exceptions when combining algorithm is not correctly set, return error classes a-la OWL API. Deleted commented out code. Added support for denyOverrides combining algorithm in policy sets. Using supported policy sets' combining algorithms where applicable.

--- PaaSwordSecurityPolicyEngine/src/main/java/org/seerc/paasword/validator/engine/PolicySetAccessRequestsEnhancer.java
+++ PaaSwordSecurityPolicyEngine/src/main/java/org/seerc/paasword/validator/engine/PolicySetAccessRequestsEnhancer.java
@@ -35,12 +35,12 @@ public class PolicySetAccessRequestsEnhancer implements JenaModelEnhancer {
 			Individual policySet = abacPolicySets.next();
 			
 			OntClass policySetAccessRequestsPositive = this.createAccessRequestsClassForConsequent(policySet, ((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/pac#positive"));
-			((OntModel)jdsi.getModel()).createIndividual(policySetAccessRequestsPositive.getURI(), ((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/pac#AccessRequestClassFor_positive"));
-			((OntModel)jdsi.getModel()).add(policySet, ((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/combiningAlgorithms#hasAccessRequestClassFor_positive"), policySetAccessRequestsPositive);
+			((OntModel)jdsi.getModel()).createIndividual(policySetAccessRequestsPositive.getURI(), ((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/pac#PolicySetAccessRequestClassFor_positive"));
+			((OntModel)jdsi.getModel()).add(policySet, ((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/combiningAlgorithms#hasPolicySetAccessRequestClassFor_positive"), policySetAccessRequestsPositive);
 			
 			OntClass policySetAccessRequestsNegative = this.createAccessRequestsClassForConsequent(policySet, ((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/pac#negative"));
-			((OntModel)jdsi.getModel()).createIndividual(policySetAccessRequestsNegative.getURI(), ((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/pac#AccessRequestClassFor_negative"));
-			((OntModel)jdsi.getModel()).add(policySet, ((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/combiningAlgorithms#hasAccessRequestClassFor_negative"), policySetAccessRequestsNegative);
+			((OntModel)jdsi.getModel()).createIndividual(policySetAccessRequestsNegative.getURI(), ((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/pac#PolicySetAccessRequestClassFor_negative"));
+			((OntModel)jdsi.getModel()).add(policySet, ((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/combiningAlgorithms#hasPolicySetAccessRequestClassFor_negative"), policySetAccessRequestsNegative);
 		}
 	}
 
@@ -50,7 +50,7 @@ public class PolicySetAccessRequestsEnhancer implements JenaModelEnhancer {
 		List<RDFNode> policySetCAList = policySet.listPropertyValues(((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/pac#hasPolicySetCombiningAlgorithm")).toList();
 		if(policySetCAList.isEmpty())
 		{	// no combining algorithm, cannot do anything
-			throw new RuntimeException("Policy set " + policySet.toString() + " does not have combining algorithm attached.");
+			return ((OntModel)jdsi.getModel()).createClass(policySet.toString() + "NoCombiningAlgorithmError");
 		}
 		
 		// Policy Sets must have exactly one CA and it must be an Individual
@@ -61,85 +61,12 @@ public class PolicySetAccessRequestsEnhancer implements JenaModelEnhancer {
 		}
 		catch(Exception e)
 		{
-			throw new RuntimeException("Policy set " + policySet.toString() + " must have exactly one valid combining algorithm attached.");
+			return ((OntModel)jdsi.getModel()).createClass(policySet.toString() + "InvalidCombiningAlgorithmError");
 		}
 		
 		// find policies that belong to this policy set
 		StmtIterator abacPolicies = ((OntModel)jdsi.getModel()).listStatements(null, ((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/pac#belongsToABACPolicySet"), policySet);
 		return this.createAccessRequestsClassFor(policySet, policySetCA, abacPolicies, consequent);
-		/*while(abacPolicies.hasNext())
-		{
-			Individual abacPolicy = abacPolicies.next().getSubject().as(Individual.class);
-			if(consequent.equals(((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/pac#positive")))
-			{	// positive consequent
-				if(policySetCA.equals(((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/combiningAlgorithms#permitOverrides")))
-				{	// permit overrides
-					this.createAccessRequestsClassFor(abacPolicy, 1, policy);						
-				}
-			}
-			else if(consequent.equals(((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/pac#negative")))
-			{	// negative consequent
-				if(policySetCA.equals(((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/combiningAlgorithms#permitOverrides")))
-				{	// permit overrides
-					this.createPriorityInContext(abacPolicy, 2, policy);						
-				}
-			}
-		}*/
-		
-		// return null;
-		
-		/*
-		// Get all Rules of Policy
-		List<RDFNode> policyRules = policySet.listPropertyValues(((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/pwd#hasRule")).toList();
-		List<RDFNode> preRIntersectionComplementUnionPreR2List = new ArrayList<RDFNode>();
-		for(RDFNode policyRuleNode:policyRules)
-		{
-			Individual r = policyRuleNode.as(Individual.class);
-			
-			// find priority of r in context of the policy
-			RDFNode rPriority = this.findPriorityInContext(policySet, r);
-			
-			// find authorisation of rule
-			Individual authorisationR = r.listPropertyValues(((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/pac#hasAuthorisation")).next().as(Individual.class);
-			if(authorisationR.equals(consequent))
-			{	// con(r) = x
-				// get antecedent of rule - pre(r)
-				RDFNode preR = r.listPropertyValues(((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/pac#hasAntecedent")).next();
-				// Find rules r' that have different consequent and have smaller priority in the same context
-				List<RDFNode> r2List = new ArrayList<RDFNode>();
-				for(RDFNode r2Node:policyRules)
-				{
-					Individual r2 = r2Node.as(Individual.class);
-					Individual authorisationR2 = r2.listPropertyValues(((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/pac#hasAuthorisation")).next().as(Individual.class);
-					if(!authorisationR2.equals(consequent))
-					{	// con(r′) ≠ x
-						// find priority of r2 in context of the policy
-						RDFNode r2Priority = this.findPriorityInContext(policySet, r2);
-						if(r2Priority != null && rPriority != null && Integer.valueOf(r2Priority.asLiteral().getValue().toString()) < Integer.valueOf(rPriority.asLiteral().getValue().toString()))
-						{	// r < r′
-							// get antecedent of r2 - pre(r′)
-							RDFNode preR2 = r2.listPropertyValues(((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/pac#hasAntecedent")).next();
-							r2List.add(preR2);
-						}
-					}
-				}
-				// create the union of r2List - ⨆ { r′ ∈ R | con(r′) ≠ x ∧ r < r′ } pre(r′ )
-				UnionClass r2Union = ((OntModel)jdsi.getModel()).createUnionClass(null, ((OntModel)this.jdsi.getModel()).createList(r2List.iterator()));
-				
-				// create the complement of the union - ¬⨆ { r′ ∈ R | con(r′) ≠ x ∧ r < r′ } pre(r′ )
-				ComplementClass r2ComplementUnion = ((OntModel)jdsi.getModel()).createComplementClass(null, r2Union);
-				
-				// create the intersection of pre(r) with the complement of the union - pre(r) ⊓ ¬⨆ { r′ ∈ R | con(r′) ≠ x ∧ r < r′ } pre(r′ )
-				List<RDFNode> preRIntersectionList = new ArrayList<RDFNode>();
-				preRIntersectionList.add(preR);
-				preRIntersectionList.add(r2ComplementUnion);
-				IntersectionClass preRIntersectionComplementUnionPreR2 = ((OntModel)jdsi.getModel()).createIntersectionClass(null, ((OntModel)this.jdsi.getModel()).createList(preRIntersectionList.iterator()));
-				preRIntersectionComplementUnionPreR2List.add(preRIntersectionComplementUnionPreR2);
-			}
-		}
-		
-		// return the union of all preRIntersectionComplementUnionPreR2s - ⨆ { r ∈ R | con(r) = x } (pre(r) ⊓ ¬⨆ { r′ ∈ R | con(r′) ≠ x ∧ r < r′ } pre(r′ ))
-		return ((OntModel)jdsi.getModel()).createUnionClass("http://www.paasword.eu/security-policy/seerc/pac#" + policySet.getLocalName() + "AccessRequestClassFor_" + consequent.getLocalName(), ((OntModel)this.jdsi.getModel()).createList(preRIntersectionComplementUnionPreR2List.iterator()));*/
 	}
 
 	private OntClass createAccessRequestsClassFor(Individual policySet, Individual policySetCA, StmtIterator abacPolicies, Resource consequent)
@@ -148,8 +75,12 @@ public class PolicySetAccessRequestsEnhancer implements JenaModelEnhancer {
 		{
 			return this.createAccessRequestsClassForPermitOverrides(policySet, abacPolicies, consequent);
 		}
+		else if(policySetCA.equals(((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/combiningAlgorithms#denyOverrides")))
+		{
+			return this.createAccessRequestsClassForDenyOverrides(policySet, abacPolicies, consequent);
+		}
 		
-		throw new RuntimeException("Unknown combining algorithm " + policySetCA.toString());
+		return ((OntModel)jdsi.getModel()).createClass(policySet.toString() + "UnsupportedCombiningAlgorithmError");
 	}
 
 	private OntClass createAccessRequestsClassForPermitOverrides(Individual policySet, StmtIterator abacPolicies, Resource consequent)
@@ -171,6 +102,30 @@ public class PolicySetAccessRequestsEnhancer implements JenaModelEnhancer {
 
 			OntClass result = ((OntModel)jdsi.getModel()).createClass("http://www.paasword.eu/security-policy/seerc/pac#" + policySet.getLocalName() + "AccessRequestClassFor_negative");
 			result.addEquivalentClass(((OntModel)jdsi.getModel()).createIntersectionClass(null, ((OntModel)this.jdsi.getModel()).createList(policyAccessRequestsForNegativeList.iterator())));
+			
+			return result;
+		}
+	}
+
+	private OntClass createAccessRequestsClassForDenyOverrides(Individual policySet, StmtIterator abacPolicies, Resource consequent)
+	{
+		if(consequent.equals(((OntModel)jdsi.getModel()).createResource("http://www.paasword.eu/security-policy/seerc/pac#negative")))
+		{	// negative consequent - R PS DO ,Deny ≡ ⨆ i≤n R i,Deny
+			OntClass result = ((OntModel)jdsi.getModel()).createClass("http://www.paasword.eu/security-policy/seerc/pac#" + policySet.getLocalName() + "AccessRequestClassFor_negative");
+			result.addEquivalentClass(this.createUnionOfDenyPolicyAccessRequests(policySet, abacPolicies));
+			return result;
+			
+		}
+		else
+		{	// positive consequent - R PS DO ,Permit ≡ (⨆ i≤n R i,Permit ) ⨅ ¬R PS DO ,Deny
+			OntClass unionOfPermitPolicyAccessRequests = this.createUnionOfPermitPolicyAccessRequests(policySet, abacPolicies);
+			OntClass unionOfDenyPolicyAccessRequests = this.createUnionOfDenyPolicyAccessRequests(policySet, abacPolicies);
+			List<RDFNode> policyAccessRequestsForPositiveList = new ArrayList<RDFNode>();
+			policyAccessRequestsForPositiveList.add(unionOfPermitPolicyAccessRequests);
+			policyAccessRequestsForPositiveList.add(((OntModel)jdsi.getModel()).createComplementClass(null, unionOfDenyPolicyAccessRequests));
+
+			OntClass result = ((OntModel)jdsi.getModel()).createClass("http://www.paasword.eu/security-policy/seerc/pac#" + policySet.getLocalName() + "AccessRequestClassFor_positive");
+			result.addEquivalentClass(((OntModel)jdsi.getModel()).createIntersectionClass(null, ((OntModel)this.jdsi.getModel()).createList(policyAccessRequestsForPositiveList.iterator())));
 			
 			return result;
 		}
@@ -199,21 +154,4 @@ public class PolicySetAccessRequestsEnhancer implements JenaModelEnhancer {
 		}
 		return ((OntModel)jdsi.getModel()).createUnionClass("http://www.paasword.eu/security-policy/seerc/pac#" + policySet.getLocalName() + "AccessRequestClassFor_negative", ((OntModel)this.jdsi.getModel()).createList(policyAccessRequestsForNegativeList.iterator()));
 	}
-
-	/*private RDFNode findPriorityInContext(Individual context, Individual r) {
-		// first get all priorities in all contexts
-		NodeIterator rPrioritiesInContexts = r.listPropertyValues(((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/list#hasPriorityInContext"));
-		while(rPrioritiesInContexts.hasNext())
-		{
-			Individual rPriorityInContext = rPrioritiesInContexts.next().as(Individual.class);
-			// get its context
-			Individual rInContext = rPriorityInContext.listPropertyValues(((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/list#inContext")).next().as(Individual.class);
-			if(rInContext.equals(context))
-			{	// we have the correct context, return the priority
-				return rPriorityInContext.listPropertyValues(((OntModel)jdsi.getModel()).createProperty("http://www.paasword.eu/security-policy/seerc/list#hasPriority")).next();
-			}
-		}
-		// couldn't find priority in context
-		return null;
-	}*/
 }
